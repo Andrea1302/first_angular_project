81d0ef9623edab3df8ecf49a9aabe803faf4bd39
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2023-01-22 19:16:21 +0100

    end of tutorial ( rif. angular )

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,17 +1,42 @@
 import { Component, Input } from '@angular/core';
 import { Hero } from '../hero';
-
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.scss']
+  styleUrls: ['./hero-detail.component.scss'],
 })
 export class HeroDetailComponent {
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
   @Input() selectedHero?: Hero;
   // @Input() handleShowHeroDetail!: () => void;
 
-  handleShowHeroDetail = () => {
-    this.selectedHero = undefined
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService
+      .getHero(id)
+      .subscribe((hero) => (this.selectedHero = hero));
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+  save(): void {
+    if (this.selectedHero) {
+      this.heroService
+        .updateHero(this.selectedHero)
+        .subscribe(() => this.goBack());
+    }
   }
 }
